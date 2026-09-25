de72b1a7a2304b8c13e5dce5419093cca87ce885
lrm-lgtm <leonardo.onipres@gmail.com>
2026-09-24 23:02:28 -0300

    chore: refresh PWA cache v15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="refrigerista-v14";
-const ASSETS=["./index.html","./styles.css?v=14","./app.js?v=14","./manifest.webmanifest","./icon.svg"];
+const CACHE="refrigerista-v15";
+const ASSETS=["./index.html","./styles.css?v=15","./app.js?v=15","./cloud.js?v=15","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
